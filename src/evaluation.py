6e7defa89cb088c2b8d6d0ccc9eabe7a85383e7b
sraimund <sraimund@ethz.ch>
2023-12-13 12:20:23 +0100

    added accuracy, precision, recall, F1 score as metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,7 +7,7 @@ from helper_functions import mkdir_if_not_exists
 
 wrong_prediction_threshold = 11
 model_names = config.model_names
-"""
+
 task_name = config.maps_non_maps_task_name
 image_input_folder = config.maps_non_maps_eval_image_folder
 prediction_names = config.maps_non_maps_prediction_names
@@ -18,7 +18,7 @@ task_name = config.pictorial_maps_other_maps_task_name
 image_input_folder = config.pictorial_maps_other_maps_eval_image_folder
 prediction_names = config.pictorial_maps_other_maps_prediction_names
 class_names = config.pictorial_maps_other_maps_class_names
-
+"""
 
 incorrect_images = {}
 accuracies = {}
@@ -33,27 +33,48 @@ for model_name in model_names:
         prediction_file_path = config.get_predictions_file_path(task_name, model_name, prediction_name)
         data = json.load(open(prediction_file_path))
         
-        correct_detections = 0
-        counter = 0
+        results = {
+            "true": {
+                "positives": 0,
+                "negatives": 0
+            },
+            "false": {
+                "positives": 0,
+                "negatives": 0                
+            }
+        }
          
         for class_index, class_name in enumerate(class_names):
             if (class_index == 0):
                 is_prediction_correct = lambda score: score > 0.5
+                result_type = "positives"
             else :
                 is_prediction_correct = lambda score: score < 0.5
+                result_type = "negatives"
             
             for image_name, predictions in data[class_name].items():
                 for prediction in predictions: # contains the predictions of three runs
                     if (is_prediction_correct(prediction["score"])):
-                        correct_detections += 1 
+                        results["true"][result_type] += 1
                     else :
+                        results["false"][result_type] += 1
                         incorrect_images[class_name].append(image_name)
-    
-                    counter += 1
         
         print(prediction_name)
-        print(round(correct_detections / counter * 100, 2))
-
+        correct_results = results["true"]["positives"] + results["true"]["negatives"]
+        incorrect_results = results["false"]["positives"] + results["false"]["negatives"]
+        accuracy = correct_results / (correct_results + incorrect_results)
+        
+        precision = results["true"]["positives"] / (results["true"]["positives"] + results["false"]["positives"])
+        recall = results["true"]["positives"] / (results["true"]["positives"] + results["false"]["negatives"])
+        f1_score = 2 * precision * recall / (precision + recall)
+        
+        print("Accuracy: ", round(accuracy * 100, 2))
+        print("Precision: ", round(precision * 100, 2))
+        print("Recall: ", round(recall * 100, 2))
+        print("F1 score: ", round(f1_score * 100, 2))
+        print()
+"""
 for class_name in class_names: 
     occurences = Counter(incorrect_images[class_name])
     
@@ -63,3 +84,4 @@ for class_name in class_names:
             mkdir_if_not_exists(image_output_folder)
             input_image_path = os.path.join(image_input_folder, class_name, image_name)
             shutil.copy(input_image_path, image_output_folder)
+"""
